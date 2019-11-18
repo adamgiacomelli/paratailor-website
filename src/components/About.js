be886ccbef81818a0115fd09a3f91315181a2c54
Adam Giacomelli <adam.giacom@gmail.com>
2019-11-18 22:24:40 +0100

    linter

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
 
 export default class About extends Component {
-    render () {
-        return (
-            <Fragment>
-                <h1>About</h1>
-                <h2>I'm a developer</h2>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <h1>About</h1>
+        <h2>I'm a developer</h2>
+      </Fragment>
+    );
+  }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Navbar extends Component {
-    render () {
+  render() {
+    const style = {
+      marginRight: 10,
+      color: "black"
+    };
 
-        const style = {
-            marginRight: 10,
-            color: 'black'
-        }
-
-        return (
-            <div>
-                <NavLink to="/" style={style}>Home</NavLink>
-                <NavLink to="/about" style={style}>About</NavLink>
-                <NavLink to="/portfolio" style={style}>Portfolio</NavLink>
-                <NavLink to="/hire-me" style={style}>Hire</NavLink>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <NavLink to="/" style={style}>
+          Home
+        </NavLink>
+        <NavLink to="/about" style={style}>
+          About
+        </NavLink>
+        <NavLink to="/portfolio" style={style}>
+          Portfolio
+        </NavLink>
+        <NavLink to="/hire-me" style={style}>
+          Hire
+        </NavLink>
+      </div>
+    );
+  }
 }
